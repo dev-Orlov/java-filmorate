3e2daa03fdb0208feba3e940e5b0443443ab2062
Orlov <alexx4456@yandex.ru>
2022-12-01 02:55:15 +0300

    первая версия ТЗ 11

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,27 @@
 package ru.yandex.practicum.filmorate;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.model.user.User;
+import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 @SpringBootTest
-class FilmorateApplicationTests {
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class FilmoRateApplicationTests {
+	private final UserDbStorage userStorage;
 
 	@Test
-	void contextLoads() {
-	}
+	public void testFindUserById() {
 
+		User user = userStorage.getUser(1);
+
+		assertThat(user).hasFieldOrPropertyWithValue("id", 1);
+	}
 }
